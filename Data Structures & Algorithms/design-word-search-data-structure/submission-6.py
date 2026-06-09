71d1c2b6a09447f1329ceadccd924a2f73b047b6
Brendan Sick <164306585+bsick223@users.noreply.github.com>
2026-06-09 04:29:34 -0700

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self): 
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+
+            curr = curr.children[c]
+
+        curr.isWord = True
+
+    def search(self, word: str) -> bool: 
+
+        def dfs(j, root):
+
+            curr = root
+
+            for i in range(j, len(word)):
+
+                c = word[i]
+                if c == ".":
+                    for child in curr.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                
+                else:
+                    if c in curr.children:
+                        curr = curr.children[c]
+                    else:
+                        return False
+                
+
+            return curr.isWord
+
+            
+
+        return dfs(0, self.root)
+        
